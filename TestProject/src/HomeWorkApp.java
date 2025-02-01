ce6269de5e74f849796b848ba3dc2956d7993476
Ekaterina <Ekaterina@mail.ru>
2025-02-01 21:08:38 +0300

    Add Homework 1

--- TestProject/src/HomeWorkApp.java
+++ TestProject/src/HomeWorkApp.java
@@ -3,46 +3,42 @@ public class HomeWorkApp {
     public static void main(String[] args) {
 
         printThreeWords();
-        checkSumSign (10,-1);
-        printColor (1);
-        compareNumbers (100,105);
+        checkSumSign(10, -1);
+        printColor(1);
+        compareNumbers(100, 105);
 
     }
 
-    public static void  printThreeWords() {
-        System.out.println ("Orange");
-        System.out.println ("Banana");
-        System.out.println ("Apple");
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
     }
 
-    public static void  checkSumSign(int a, int b) {
-        int c = a+b;
-        if (c>=0){
-            System.out.println ("Сумма положительная");
-        }
-        else {
-            System.out.println ("Сумма отрицательная");
+    public static void checkSumSign(int a, int b) {
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
         }
     }
 
     public static void printColor(int value) {
-        if (value <=0){
-            System.out.println ("Красный");
-        }
-        else if (value>100) {
-            System.out.println ("Зеленый");
-        }
-        else{
-            System.out.println ("Желтый");
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
+        } else {
+            System.out.println("Желтый");
         }
     }
 
-    public static void compareNumbers(int a, int b){
-        if (a>=b) {
-            System.out.println ("a>=b");
-        }
-        else{
-            System.out.println ("a<b");
+    public static void compareNumbers(int a, int b) {
+        if (a >= b) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
         }
     }
 }
